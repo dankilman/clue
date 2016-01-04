4c11ab8086afe190099748088d35879e19686087
Dan Kilman <dankilman@gmail.com>
2016-01-04 13:35:22 +0200

    add licenses

--- cdev/output.py
+++ cdev/output.py
@@ -1,3 +1,19 @@
+########
+# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############
+
 import colors
 
 from workflowcmd.output import Event
@@ -8,10 +24,10 @@ class GitStatusEvent(object):
     @staticmethod
     def factory(env, verbose):
         class GitStatusEventImpl(Event):
-
             def __str__(self):
-                if (not verbose and self.level and self.level.lower() == 'info'
-                        and self.node_name):
+                if (not verbose and self.level and
+                        self.level.lower() == 'info' and
+                        self.node_name):
                     node = env.storage.get_node(self.node_name)
                     repo = node.properties['name']
                     repo = colors.green('{0:<30}'.format(repo))
@@ -26,7 +42,6 @@ class NoseEvent(object):
     @staticmethod
     def factory(env, verbose):
         class NoseEventImpl(Event):
-
             def __str__(self):
                 if not verbose:
                     return self.message
--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
+########
+# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############
+
 from setuptools import setup
 
 setup(
     name='cdev',
     version='0.1',
-    author='Dan Kilman',
-    author_email='dankilman@gmail.com',
+    author='GigaSpaces',
+    author_email='cosmo-admin@gigaspaces.com',
     packages=['cdev'],
-    description='cloudify-dev CLI',
+    description='Cloudify development environment CLI',
     zip_safe=False,
     install_requires=[
         'sh',
@@ -17,6 +33,5 @@ setup(
         'console_scripts': [
             'cdev = cdev:main',
         ]
-    },
-
+    }
 )
