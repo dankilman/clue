53e49b082c4cb944b1243ebcfa6e3cbd6aff0be0
Dan Kilman <dankilman@gmail.com>
2016-01-15 22:08:10 +0200

    improve blueprint descriptions and upgrade to clash 0.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='clue',
-    version='0.12',
+    version='0.13',
     author='GigaSpaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['clue'],
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'sh',
         'virtualenvwrapper',
-        'clash==0.4'
+        'clash==0.5'
     ],
     include_package_data=True,
     entry_points={
