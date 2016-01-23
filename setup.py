5d230951a209b595e1924a2b4153f04717e148f7
Dan Kilman <dankilman@gmail.com>
2016-01-23 02:42:54 +0200

    update clash to 0.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='clue',
-    version='0.22',
+    version='0.23',
     author='GigaSpaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['clue'],
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'sh',
         'virtualenvwrapper',
-        'clash==0.7'
+        'clash==0.8'
     ],
     include_package_data=True,
     entry_points={
